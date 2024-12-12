98c546edbe3f519f455b943c47bcd9fdfc138749
Ugo Stephant <ugo@poool.fr>
2024-12-12 09:39:03 +0100

    fix: vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: './src',
+  base: process.env.NODE_ENV === 'production'
+    ? '/website/'
+    : '',
   build: {
     outDir: '../dist',
   },
